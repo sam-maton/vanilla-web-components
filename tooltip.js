1d0d48a86bbbaac53872f988a61151ef14a59963
smaton1 <sam.maton91@gmail.com>
2025-03-23 22:07:36 +0000

    setup basic web component

--- tooltip.js
+++ tooltip.js
@@ -1,1 +1,8 @@
-console.log('loaded')
+class Tooltip extends HTMLElement{
+  constructor(){
+    super()
+    console.log('this is working');
+  }
+}
+
+customElements.define('dev-tooltip', Tooltip)
