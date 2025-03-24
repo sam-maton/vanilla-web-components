b7a87dee592fb3b41e9b3330f302cb1ba47ce5fa
smaton1 <sam.maton91@gmail.com>
2025-03-24 10:52:08 +0000

    setup event listeners

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,26 @@
 class Tooltip extends HTMLElement{
   constructor(){
     super()
+    this.tooltipContainer;
   }
 
   connectedCallback(){
     const tooltipIcon = document.createElement('span')
     tooltipIcon.textContent = '(?)'
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
     this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip(){
+    console.log(this);
+    this.tooltipContainer = document.createElement('div')
+    this.tooltipContainer.textContent = 'This is the tooltip text'
+    this.appendChild(this.tooltipContainer)
+  }
+  
+  _hideTooltip(){
+    this.removeChild(this.tooltipContainer)
   }
 }
 
