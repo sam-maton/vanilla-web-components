38d58dee07cd1e38742fdf90a2a703b4f9a016e7
smaton1 <sam.maton91@gmail.com>
2025-03-24 21:48:47 +0000

    tidied up implementation

--- info-button.js
+++ info-button.js
@@ -2,37 +2,43 @@ class InfoButton extends HTMLElement {
   constructor() {
     super();
 
-    this._info;
-    this._visible = false;
-
     this.attachShadow({
       mode: "open"
     });
 
     this.shadowRoot.innerHTML = `
       <style>
-        p{
+        #info-box{
           display: none;
         }
       </style>
       <button>Toggle</button>
       <p id="info-box"><slot></slot></p>
     `;
+
+    this._info = this.shadowRoot.querySelector("#info-box");
+    this._visible = false;
+    this._button = this.shadowRoot.querySelector("button");
+    this._button.addEventListener("click", this._toggleInfo.bind(this));
   }
 
   connectedCallback() {
-    this._info = this.shadowRoot.querySelector("p");
-    const button = this.shadowRoot.querySelector("button");
-    button.addEventListener("click", this._toggleInfo.bind(this));
+    console.log(this.getAttribute("is-visible"));
+    if (
+      this.hasAttribute("is-visible") &&
+      this.getAttribute("is-visible") === "true"
+    ) {
+      this._visible = true;
+      this._info.style.display = "block";
+    }
   }
 
   _toggleInfo() {
-    if (!this._visible) {
+    this._visible = !this._visible;
+    if (this._visible) {
       this._info.style.display = "block";
-      this._visible = true;
     } else {
       this._info.style.display = "none";
-      this._visible = false;
     }
   }
 }
